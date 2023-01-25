1177a7ce1d3a5b552e0e64df7fb24cf59d2d659c
ivankrn <kropotiniv@yandex.ru>
2023-01-25 10:45:07 +0500

    Init commit

---
+++ src/main/java/com/ivankrn/transport_tracker_bot/TransportTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package com.ivankrn.transport_tracker_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportTrackerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportTrackerBotApplication.class, args);
+	}
+
+}
